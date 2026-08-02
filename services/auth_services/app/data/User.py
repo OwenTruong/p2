a13f6617927d059c42178acdb7227f3eaaf3ef24
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2026-08-02 05:15:14 +0000

    refactor: changed save() method in user repository to mirror spring jpa repository's save() method a bit more

--- services/auth_services/app/data/User.py
+++ services/auth_services/app/data/User.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 from typing import Literal
 
 class User(BaseModel):
-  user_id: int = Field()
+  user_id: int | None  = Field(default=None) # 0 represents that the user is new and does not exist in db yet. BIGSERIAL start generating id at 1.
   email: str = Field(examples=["john@example.com"])
   password_hash: str = Field()
   first_name: str = Field(examples=["John"])
--- services/auth_services/app/repositories/user_repository.py
+++ services/auth_services/app/repositories/user_repository.py
@@ -27,14 +27,18 @@ class UserRepository(DBRepository):
 
     super().__init__(table_name, table_query)
 
-  def save(self, email: str, password_hash: str, first_name: str, last_name: str) -> User:
-    query = f"INSERT INTO {self._table_name} (email, password_hash, first_name, last_name) VALUES(%s, %s, %s, %s) RETURNING *;"
-    return self._execute_fetch_one(query, User, values=[email, password_hash, first_name, last_name])
+  def save(self, user: User) -> User:
+    if user.user_id == None:
+      query = f"INSERT INTO {self._table_name} (email, password_hash, first_name, last_name) VALUES(%s, %s, %s, %s) RETURNING *;"
+      return self._execute_fetch_one(query, User, values=[user.email, user.password_hash, user.first_name, user.last_name])
+    else:
+      query = f"UPDATE {self._table_name} SET email=%s,password_hash=%s,first_name=%s,last_name=%s,status=%s WHERE user_id=%s RETURNING *;"
+      return self._execute_fetch_one(query, User, values=[user.email, user.password_hash, user.first_name, user.last_name, user.status, user.user_id])
 
-  def find_by_id(self, users_id: int) -> User:
+  def find_by_id(self, id: int) -> User:
     query = f"SELECT * FROM {self._table_name} WHERE users_id=%s"
-    return self._execute_fetch_one(query, User, values=[users_id])
+    return self._execute_fetch_one(query, User, values=[id])
 
-  def delete_by_id(self, users_id: int) -> None:
+  def delete_by_id(self, id: int) -> None:
     query = f"DELETE FROM {self._table_name} WHERE users_id=%s"
-    return self._execute(query, values=[users_id])
+    return self._execute(query, values=[id])
