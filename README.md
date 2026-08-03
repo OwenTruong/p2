b85c6681c11a31268ca3a5ee0f215153a83e086f
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2026-08-03 20:30:06 +0000

    added create_db.sh for creating 3 different db for a single postgresql docker compose service for local testing; bugfix migrate script

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 A microservices-based rental booking platform demonstrating modern cloud-native architecture, containerization, orchestration, CI/CD, and observability.
 
+## Local Development
 
-# Initializing and migrating database
+### Initializing and migrating database
 
 To initialize and migrate auth database for local development, run the following
 ```python
--- services/auth_services/app/core/config.py
+++ services/auth_services/app/core/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import logging
 
 from ..dtos.config import Config
 
@@ -7,15 +8,27 @@ load_dotenv()
 
 # intentionally using invalid defaults to find error in config if any
 __config = Config(
-  db_host = os.getenv("AUTH_DB_HOST", "10.0.3.254"),
+  db_host = os.getenv("DB_HOST", "10.0.3.254"),
   db_name = os.getenv("AUTH_DB_NAME", "my_db"),
-  db_user = os.getenv("AUTH_DB_USER", "my_db_user"),
-  db_password = os.getenv("AUTH_DB_PASSWORD", "MyPassword1234"),
-  db_port = int(os.getenv("AUTH_DB_PORT", 1000)),
+  db_user = os.getenv("DB_USER", "my_db_user"),
+  db_password = os.getenv("DB_PASSWORD", "MyPassword1234"),
+  db_port = int(os.getenv("DB_PORT", 1000)),
 
   mode = "Production" if os.getenv("MODE") == "Production" else "Development",
   password_salt = os.getenv("PASSWORD_SALT", "MySaltyPassword1234")
 )
+
+logging.debug(f"""Config Loaded:
+  db_host: {__config.db_host}
+  db_name: {__config.db_name}
+  db_user: {__config.db_user}
+  db_password: REDACTED
+  db_port: {__config.db_port}
+
+  mode: {__config.mode}
+  password_salt: REDACTED
+""")
+
 
 def get_config():
   return __config
