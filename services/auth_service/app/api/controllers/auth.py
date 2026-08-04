ed987046d1bbb203d353612f9bdb161e5ec89d2d
Subarumu <stickdoggo1@gmail.com>
2026-08-04 16:51:32 -0400

    created the register endpoint, currently unable to test changes due to an issue with docker being unable to turn on the database server

--- services/auth_service/app/api/controllers/auth.py
+++ services/auth_service/app/api/controllers/auth.py
@@ -1,11 +1,24 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response, status, HTTPException
 from app.dtos.auth import LoginRequest, AuthSuccessResponse
+from app.dtos.user_dto import UserCreateRequestDTO
+from app.repositories.user_repository import UserRepository
 from app.services.auth_service import AuthService
 from app.core.config import get_config
+import psycopg2
 
 config = get_config()
 router = APIRouter(prefix="/api/auth", tags=["Auth"])
 auth_service = AuthService()
+user_repository = UserRepository()
+
+#Register Endpoint 
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+def register(dto: UserCreateRequestDTO):
+    try:
+        user_repository.save(dto)
+        return {"status": "success", "detail": f"Account for user `{dto.email}` has been provisioned"}
+    except psycopg2.IntegrityError as exc:
+        raise HTTPException(status_code=409, detail=str(exc))
 
 @router.post("/login", response_model=AuthSuccessResponse, status_code=status.HTTP_200_OK)
 def login(dto: LoginRequest, response: Response):
