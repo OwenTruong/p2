6c73b7dc730c51a473f5c73a0b2b74e89b451589
Daniel Sarzynski <dsarzynski13116@gmail.com>
2026-08-09 19:35:09 -0400

    implement database connection

---
+++ services/listing_service/app/dtos/config.py
@@ -0,0 +1,6 @@
+from pydantic import Field
+from typing import Literal
+from shared.dtos.db_config import DBConfig
+
+class Config(DBConfig):
+  mode: Literal["Production", "Development"] = Field(examples=["Production", "Development"])
---
+++ services/listing_service/app/repositories/listing_repository.py
@@ -0,0 +1,29 @@
+from app.models.listing import Listing
+from app.core.config import get_config
+from shared.repositories.db_repository import DBRepository
+
+class ListingRepository(DBRepository):
+    def __init__(self):
+        table_name = 'listings'
+        config = get_config()
+        super().__init__(table_name, config)
+
+    def save(self, listing: Listing) -> Listing:
+        query = f"""
+            INSERT INTO {self._table_name} (
+                host_id, title, description, price_per_night, 
+                max_guests, bedrooms, bathrooms, is_published, 
+                address, city, state, zip_code
+            ) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
+            RETURNING *;
+        """
+        return self._execute_fetch_one(
+            query, 
+            Listing, 
+            values=[
+                listing.host_id, listing.title, listing.description, listing.price_per_night,
+                listing.max_guests, listing.bedrooms, listing.bathrooms, listing.is_published,
+                listing.address, listing.city, listing.state, listing.zip_code
+            ]
+        )
