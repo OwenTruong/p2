5e8ab52f2489139b07a5ff751436f3fd37da1321
Daniel Sarzynski <dsarzynski13116@gmail.com>
2026-08-17 15:11:54 -0400

    adjust repo to allow setting values

--- services/listing_service/app/repositories/listing_repository.py
+++ services/listing_service/app/repositories/listing_repository.py
@@ -11,22 +11,43 @@ class ListingRepository(DBRepository):
         super().__init__(table_name, config)
 
     def save(self, listing: Listing) -> Listing:
-        query = f"""
-            INSERT INTO {self._table_name} (
-                host_id, title, description, price_per_night, 
-                max_guests, bedrooms, bathrooms, is_published, 
-                address, city, state, zip_code
-            ) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
-            RETURNING *;
-        """
+        if listing.listing_id is None: 
+            query = f"""
+                INSERT INTO {self._table_name} (
+                    host_id, title, description, price_per_night, 
+                    max_guests, bedrooms, bathrooms, is_published, 
+                    address, city, state, zip_code
+                ) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
+                RETURNING *;
+            """
+            return self._execute_fetch_one(
+                query, 
+                Listing, 
+                values=[
+                    listing.host_id, listing.title, listing.description, listing.price_per_night,
+                    listing.max_guests, listing.bedrooms, listing.bathrooms, listing.is_published,
+                    listing.address, listing.city, listing.state, listing.zip_code
+                ]
+            )
+        else: 
+            query = f"""
+                UPDATE {self._table_name} 
+                SET title=%s, description=%s, price_per_night=%s, 
+                    max_guests=%s, bedrooms=%s, bathrooms=%s, is_published=%s, 
+                    address=%s, city=%s, state=%s, zip_code=%s 
+                WHERE listing_id=%s 
+                RETURNING *;
+            """
+            
         return self._execute_fetch_one(
             query, 
             Listing, 
             values=[
-                listing.host_id, listing.title, listing.description, listing.price_per_night,
+                listing.title, listing.description, listing.price_per_night,
                 listing.max_guests, listing.bedrooms, listing.bathrooms, listing.is_published,
-                listing.address, listing.city, listing.state, listing.zip_code
+                listing.address, listing.city, listing.state, listing.zip_code,
+                listing.listing_id
             ]
         )
     
